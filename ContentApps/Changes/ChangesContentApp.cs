f9bed8ab72aabd1e6e585320197954f05b0a1cd4
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2022-01-21 13:54:12 +0100

    Register contentapp from code and only show if source is IContent

---
+++ ContentApps/Changes/ChangesContentApp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.ContentEditing;
+using Umbraco.Core.Models.Membership;
+
+namespace Umbraco.Web.UI.ContentApps.Changes
+{
+    public class ChangesContentApp : IContentAppFactory
+    {
+        public ContentApp GetContentAppFor(object source, IEnumerable<IReadOnlyUserGroup> userGroups)
+        {
+            if (!(source is IContent)) 
+                return null;
+            
+            var changesApp = new ContentApp
+            {
+                Alias = "changes",
+                Name = "Changes",
+                Icon = "icon-edit",
+                View = "/App_Plugins/Changes/app.html",
+                Weight = 0
+
+            };
+
+            return changesApp;
+
+        }
+    }
+}
